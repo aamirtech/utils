8ac4350e366cce2539f9a806f13260e02b91ba38
Aamir Khan <aamirmakofficial@gmail.com>
2024-10-16 00:21:17 +0530

    added jsdoc and added dist to gitignore

--- dist/getInitials.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const GetInitials: (name: string) => string;
--- dist/toCase.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const toTitleCase: (str: string) => string;
-export declare const toCamelCase: (str: any) => string;
-export declare const toPascalCase: (str: any) => string;
-export declare const toSnakeCase: (str: any) => string;
-export declare const toKebabCase: (str: any) => string;
